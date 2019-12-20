4e36ac653c4fbc0053e19b7228d7b77f65f333a8
abhishek-n-c-hpe <59077109+abhishek-n-c-hpe@users.noreply.github.com>
2019-12-20 10:39:14 +0530

    Add files via upload

---
+++ Abhishek_01.py
@@ -0,0 +1,32 @@
+### INPUT####
+
+# Display the Fibonacci sequence up to n-th term
+
+nterms = int(input("How many terms? "))
+
+### Process ###
+
+##Input the first 2 digits(numbers
+
+n1, n2 = 0, 1
+count = 0
+
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
+       
+print("Above are the Fibonacci Numbers")
+    
---
+++ Abhishek_03.py
@@ -0,0 +1,61 @@
+import pyautogui, time
+
+class saplogonBOT(object):
+	def __init__(self,usr,pw,inst,tc):
+		self.userid=usr
+		self.password=pw
+		self.instance=inst
+		self.tco=tc
+
+	def activate_preinvoke(self):
+		pyautogui.hotkey('win', 'd')
+
+
+	def activate_saplogon(self):
+		try:
+			pyautogui.hotkey('win','r')
+			pyautogui.typewrite('saplogon')
+			pyautogui.press('enter')
+
+			return True
+		except:
+			return False
+
+	def activate_postinvoke(self):
+		pyautogui.hotkey('win','up')
+
+	def open_instance(self):
+		try:
+			pyautogui.hotkey('ctrl','f')
+			pyautogui.typewrite(self.instance)
+			pyautogui.press('enter')
+			return True
+		except:
+			return False
+	def sap_login(self):
+		pyautogui.typewrite(self.userid)
+		pyautogui.press('tab')
+		pyautogui.typewrite(self.password)
+		pyautogui.press('enter')
+		time.sleep(1)
+		pyautogui.press('tab')
+		pyautogui.press('enter')
+		pyautogui.press('enter')
+
+
+############################################
+
+if __name__=='__main__':
+	mysap=saplogonBOT('XXX','xxxxx','xxx','xxxx')
+
+	mysap.activate_preinvoke()
+	time.sleep(2)
+	mysap.activate_saplogon()
+	time.sleep(4)
+	mysap.activate_postinvoke()
+	time.sleep(5)
+	mysap.open_instance()
+	time.sleep(5)
+	mysap.sap_login()
+	time.sleep(7)
+	
